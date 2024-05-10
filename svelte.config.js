c59654f73f2b0d249cec0a39608e8137b2f2af7f
ejpadaen <20230060@mywhitecliffe.com>
2024-05-11 00:07:59 +1200

    strict false in adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			strict: false
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
