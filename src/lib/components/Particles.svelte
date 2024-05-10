5b3a2a7432e776943c79429b526bd8b18526f44d
ejpadaen <20230060@mywhitecliffe.com>
2024-05-11 11:52:39 +1200

    load particles script with onmount

--- src/lib/components/Particles.svelte
+++ src/lib/components/Particles.svelte
@@ -1,7 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+     
+    onMount(() => {
+            console.log('attaching particles js');
+        const script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js'; // might be outdated so check for an update can link
+        script.onload = () => {
+            console.log('loading particles.js');
+            particlesJS.load('particles-js', '/path/to/your/particles.json', () => {
+            console.log('particles.js loaded');
+         });
+       };
+       document.body.appendChild(script);
+     });
+</script>
+
 <html lang="en">
     <div id="particles-js"></div>
 
-    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
     <script src="assets/app.js"></script>
 </html>
 
