ba55189bb9d4f61fdd493bd16e83e703b8132a8c
ejpadaen <20230060@mywhitecliffe.com>
2024-05-11 00:19:52 +1200

    nah back to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 	}
 };
 
