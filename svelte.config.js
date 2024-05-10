7e3e9e9c789c91cbee634cbc948341dd895fd27d
ejpadaen <20230060@mywhitecliffe.com>
2024-05-11 00:16:07 +1200

    change adapter to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			strict: false
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
+		adapter: adapter(),
 	}
 };
 
